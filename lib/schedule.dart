a28e76fffb8496c72761cb7178ae0841c1664dfb
DIP-SARKER <sarker15-5274@diu.edu.bd>
2025-03-09 22:27:49 +0600

    schedule page

---
+++ lib/schedule.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+class Schedule extends StatefulWidget {
+  const Schedule({super.key});
+
+  @override
+  State<Schedule> createState() => _Schedule();
+}
+
+class _Schedule extends State<Schedule> {
+  int selectedDateIndex = 0;
+  final List<Map<String, String>> schedule = [
+    {
+      "time": "06:30 AM",
+      "route": "DSC to Tongi",
+      "bus": "Surjomukhi 100",
+      "driver": "Abdul Karim"
+    },
+    {
+      "time": "08:00 AM",
+      "route": "DSC to Rajlokkhi",
+      "bus": "Surjomukhi 101",
+      "driver": "Rahim Uddin"
+    },
+    {
+      "time": "09:00 AM",
+      "route": "Tongi to DSC",
+      "bus": "Oporajita 102",
+      "driver": "Sohail Rana"
+    },
+    {
+      "time": "10:30 AM",
+      "route": "Rajlokkhi to DSC",
+      "bus": "Oporajita 103",
+      "driver": "Hasan Mahmud"
+    },
+    {
+      "time": "12:00 PM",
+      "route": "DSC to Dhanmondi",
+      "bus": "Surjomukhi 104",
+      "driver": "Shafiqul Islam"
+    },
+    {
+      "time": "01:30 PM",
+      "route": "Dhanmondi to DSC",
+      "bus": "Oporajita 105",
+      "driver": "Mizanur Rahman"
+    },
+    {
+      "time": "03:00 PM",
+      "route": "DSC to Mirpur",
+      "bus": "Surjomukhi 106",
+      "driver": "Kamrul Hasan"
+    },
+    {
+      "time": "04:30 PM",
+      "route": "Mirpur to DSC",
+      "bus": "Oporajita 107",
+      "driver": "Faruq Ahmed"
+    },
+    {
+      "time": "05:30 PM",
+      "route": "DSC to Rampura",
+      "bus": "Surjomukhi 108",
+      "driver": "Jahangir Alam"
+    },
+    {
+      "time": "07:00 PM",
+      "route": "Rampura to DSC",
+      "bus": "Oporajita 109",
+      "driver": "Nasir Uddin"
+    },
+    {
+      "time": "08:30 PM",
+      "route": "DSC to Narayanganj",
+      "bus": "Surjomukhi 110",
+      "driver": "Alamgir Hossain"
+    },
+    {
+      "time": "09:30 PM",
+      "route": "Narayanganj to DSC",
+      "bus": "Oporajita 111",
+      "driver": "Rakib Hasan"
+    },
+    {
+      "time": "11:00 PM",
+      "route": "DSC to Tongi",
+      "bus": "Surjomukhi 112",
+      "driver": "Shahadat Hossain"
+    },
+    {
+      "time": "12:30 AM",
+      "route": "Tongi to DSC",
+      "bus": "Oporajita 113",
+      "driver": "Belal Ahmed"
+    }
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Transport Schedule"),
+        toolbarHeight: 100,
+        actions: [
+          IconButton(
+              icon: Icon(HeroIcons.arrow_path_rounded_square),
+              onPressed: () {}),
+          IconButton(icon: Icon(HeroIcons.bars_4), onPressed: () {}),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: schedule.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 25),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  color: Colors.grey[900],
+                  child: ListTile(
+                    contentPadding: EdgeInsets.all(16),
+                    title: Text(
+                      schedule[index]['route']!,
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 5),
+                        Text("Time: ${schedule[index]['time']}",
+                            style: TextStyle(color: Colors.grey)),
+                        Text("Bus: ${schedule[index]['bus']}",
+                            style: TextStyle(color: Colors.grey)),
+                        Text("Driver: ${schedule[index]['driver']}",
+                            style: TextStyle(color: Colors.grey)),
+                      ],
+                    ),
+                    leading:
+                        Icon(FontAwesome.bus_solid, color: Colors.blueAccent),
+                    trailing: IconButton(
+                      icon:
+                          Icon(Icons.info_outline_rounded, color: Colors.white),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            backgroundColor: Colors.grey[900],
+                            title: Text("Bus Details"),
+                            content: Text(
+                              "Bus: ${schedule[index]['bus']}\nDriver: ${schedule[index]['driver']}\nTime: ${schedule[index]['time']}\nRoute: ${schedule[index]['route']}",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.pop(context),
+                                child: Text("Close",
+                                    style: TextStyle(color: Colors.blueAccent)),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
