16a618dbe76da3c5003b7d11e4d1f20ed515e52d
Sumiara5365 <rafiq15-5365@diu.edu.bd>
2025-03-15 13:54:51 +0600

    changed font color

--- lib/splashpage.dart
+++ lib/splashpage.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
 import 'package:routeradar/schedule.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -14,7 +14,7 @@ class _SplashScreen extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer.periodic(Duration(seconds: 5), (timer) {
+    Timer.periodic(Duration(seconds: 4), (timer) {
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => Schedule()));
     });
@@ -23,59 +23,50 @@ class _SplashScreen extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 119, 82, 223),
       body: Center(
-        child: DefaultTextStyle(
-          style: const TextStyle(
-            fontSize: 50.0,
-            fontFamily: 'LondrinaSketch',
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-          child: AnimatedTextKit(
-            animatedTexts: [
-              TyperAnimatedText(
-                'R o u t e R a d a r',
-                speed: const Duration(milliseconds: 100),
-              ),
-              WavyAnimatedText(
-                'R o u t e R a d a r',
-                speed: const Duration(milliseconds: 90),
-              ),
+        child: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Colors.blue,
+              Colors.purple,
             ],
-            totalRepeatCount: 1,
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          )),
+          child: DefaultTextStyle(
+            style: const TextStyle(
+              fontSize: 50.0,
+              fontFamily: 'LondrinaSketch',
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+            child: AnimatedTextKit(
+              animatedTexts: [
+                ColorizeAnimatedText(
+                  'R o u t e R a d a r',
+                  textStyle: const TextStyle(
+                    fontSize: 50.0,
+                    fontFamily: 'LondrinaSketch',
+                    fontWeight: FontWeight.bold,
+                  ),
+                  colors: [
+                    Colors.white,
+                    Colors.blue,
+                    Colors.yellow,
+                    Colors.orange,
+                    Colors.red,
+                  ],
+                  speed: const Duration(milliseconds: 500),
+                ),
+              ],
+              totalRepeatCount: 1,
+              isRepeatingAnimation: false,
+            ),
           ),
         ),
       ),
     );
   }
 }
-          
-          
-        //   AnimatedTextKit(
-        //     animatedTexts: [
-        //       TyperAnimatedText(
-        //         'R o u t e R a d a r',
-        //         textStyle: const TextStyle(
-        //             fontSize: 60.0,
-        //             fontFamily: "LondrinaSketch",
-        //             fontWeight: FontWeight.bold,
-        //             ),
-        //         // speed: const Duration(milliseconds: 100),
-        //       ),
-        //     ],
-        //     // totalRepeatCount: 3,
-        //     // pause: const Duration(milliseconds: 1500),
-        //     // displayFullTextOnTap: false,
-        //     //controller: myAnimatedTextController),
-        //   ),
-        // ));
-
-
-
-// 'R o u t e R a d a r',
-//       textStyle: TextStyle(
-// fontFamily: 'LondrinaSketch',
-//                 fontSize: 60,
-//                 fontWeight: FontWeight.bold,
-//                 color: Theme.of(context).primaryColor)
